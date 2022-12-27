3ed3851c1429bcf8edb9273a88dccf8c74a5b914
vmarshirov <vmarshirov@outlook.com>
2022-12-27 10:09:42 +0300

    urlProect - views

--- django010Project/urls.py
+++ django010Project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
     path('helloApp/', include("helloApp.urls")),
     path('renderApp/', include("renderApp.urls")),
-    path('', include("renderApp.urls")),
+    # path('', include("renderApp.urls")),
 ]
---
+++ django010Project/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+
+# from django.shortcuts import render
+# Create your views here.
+
+# http://127.0.0.1:8000/
+
+def index(request):
+    return HttpResponse("hello010Project")
