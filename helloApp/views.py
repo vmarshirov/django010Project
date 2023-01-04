e753d4896dd83eb7f5c94eba3bd030909f4404e3
vmarshirov <vmarshirov@outlook.com>
2023-01-04 18:11:41 +0300

    helloApp/views.py - path_element

--- helloApp/views.py
+++ helloApp/views.py
@@ -21,14 +21,19 @@ def abc(request):
 
 
 def f_str(request, str_value):
-    print("type(request), request: ", type(request), request)
+    print("type(request), request: ", type(request), request.path)
+    print("request.META:", request.META.get("HTTP_HOST"))
+    path_elements = request.path.split("/")
+    elements_amount = path_elements.__len__()
+    path_element = path_elements[elements_amount - 2]
     print("type(str_value), str_value: ", type(str_value), str_value)
-    return HttpResponse(f"f_str, str_value.capitalize():  {str_value.capitalize()}")
+    return HttpResponse(
+        f"<p>f_str, str_value.capitalize():  {str_value.capitalize()}; \n<br>path_element: {path_element} \n</p>")
 
 
 def f_int(request, int_value):
     print(type(int_value), int_value)
-    return HttpResponse(f"f_int, int_value: {int_value}")
+    return HttpResponse(f"f_int, int_value: {int_value} ")
 
 
 def f_slug(request, slug_value):
