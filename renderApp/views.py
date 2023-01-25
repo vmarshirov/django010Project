f3e28ada689d6440c850144ab5f99d7e0af3996a
vmarshirov <vmarshirov@outlook.com>
2023-01-25 20:35:51 +0300

    renderApp

--- renderApp/views.py
+++ renderApp/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 
-
-
-
-
-
-# Create your views here.
-
-# def index(request)
-#     return HttpResponse('renderApp')
+# https://docs.djangoproject.com/en/4.1/topics/templates/
+# https://docs.djangoproject.com/en/4.1/ref/templates/builtins/#id3
+# https://docs.djangoproject.com/en/4.1/ref/templates/language/
 
 
 def index(request): # http://127.0.0.1:8000/renderApp/
@@ -18,9 +12,9 @@ def index(request): # http://127.0.0.1:8000/renderApp/
 def greet(request, name): # http://127.0.0.1:8000/renderApp/greet/Иванов
     return render(request, "renderApp/greet.html", {"name": name})
 
-
 def page_01(request, query_str): # http://127.0.0.1:8000/renderApp/page_01
     print(request)
     print(query_str)
-
-    return render(request, "renderApp/page_01.html", {"queryStr": query_str})
+    context = {"query_str": query_str}
+    return render(request, "renderApp/page_01.html",context)
+    # return render(request, "renderApp/page_01.html", {"query_str": query_str})
